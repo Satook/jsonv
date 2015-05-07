ed154769557baf852e7791ea04876a9c7a8ead35
Andrew Stewart <code@satook.com>
2015-05-07 21:54:36 +1000

    [funcs] Added helper funcs for custom String and Array validators.

--- validators_array.go
+++ validators_array.go
@@ -9,6 +9,12 @@ type SliceValidator interface {
 	ValidateSlice(i interface{}) error
 }
 
+type SliceValidatorFunc func(i interface{}) error
+
+func (f SliceValidatorFunc) ValidateSlice(i interface{}) error {
+	return f(i)
+}
+
 func (m *MinLenV) ValidateSlice(i interface{}) error {
 	if reflect.ValueOf(i).Len() < m.l {
 		return fmt.Errorf(ERROR_MIN_LEN_ARR, m.l)
--- validators_string.go
+++ validators_string.go
@@ -9,6 +9,12 @@ type StringValidator interface {
 	ValidateString(s string) error
 }
 
+type StringValidatorFunc func(s string) error
+
+func (f StringValidatorFunc) ValidateString(s string) error {
+	return f(s)
+}
+
 func (m *MinLenV) ValidateString(s string) error {
 	if len(s) < m.l {
 		return fmt.Errorf(ERROR_MIN_LEN_STR, m.l)
@@ -27,14 +33,14 @@ type PatternV struct {
 	r *regexp.Regexp
 }
 
+func Pattern(re string) *PatternV {
+	return &PatternV{regexp.MustCompile(re)}
+}
+
 func (p *PatternV) ValidateString(s string) error {
 	if p.r.MatchString(s) {
 		return nil
 	} else {
 		return fmt.Errorf(ERROR_PATTERN_MATCH, p.r.String())
 	}
-}
-
-func Pattern(re string) *PatternV {
-	return &PatternV{regexp.MustCompile(re)}
 }
